f42b93e51f70b7219e31afaad0ab351873b6010d
Nikita Havryliuk <chinter.games1@gmail.com>
2022-12-09 18:33:43 +0200

    created handle delete function

--- src/Components/TodosList/TodosList.tsx
+++ src/Components/TodosList/TodosList.tsx
@@ -1,27 +1,38 @@
 import { MDBListGroup, MDBListGroupItem, MDBCheckbox, MDBTooltip, MDBIcon } from "mdb-react-ui-kit";
+import { useCallback } from "react";
 import { Todo } from "../../types/Todo";
 
 type Props = {
   todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 };
 
-export const TodoList: React.FC<Props> = ({ todos }) => {
+export const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
+  const handleDelete = useCallback((id: number) => {
+    const newTodos = [...todos];
+    const indexTodo = newTodos.findIndex(todo => todo.id === id)
+
+    newTodos.splice(indexTodo, 1);
+
+    setTodos(newTodos);
+  }, [setTodos, todos]);
+
   return (
     <>
-    {todos.map(todo => (
-        <MDBListGroup horizontal className="rounded-0 bg-transparent" key={todo.id}>
+    {todos.map(({id, title, completed}) => (
+        <MDBListGroup horizontal className="rounded-0 bg-transparent" key={id}>
           <MDBListGroupItem className="d-flex align-items-center ps-0 pe-3 py-1 rounded-0 border-0 bg-transparent">
             <MDBCheckbox
               name="flexCheck"
               value=""
               id="flexCheckChecked"
-              defaultChecked={todo.completed}
+              defaultChecked={completed}
             />
           </MDBListGroupItem>
           <MDBListGroupItem className="px-3 py-1 d-flex align-items-center flex-grow-1 border-0 bg-transparent">
             {" "}
             <p className="lead fw-normal mb-0">
-              {todo.title}
+              {title}
             </p>
           </MDBListGroupItem>
           <MDBListGroupItem className="ps-3 pe-0 py-1 rounded-0 border-0 bg-transparent">
@@ -40,10 +51,16 @@ export const TodoList: React.FC<Props> = ({ todos }) => {
               </MDBTooltip>
               <MDBTooltip
                 tag="a"
+                type="button"
                 wrapperProps={{ href: "#!" }}
                 title="Delete todo"
               >
-                <MDBIcon fas icon="trash-alt" color="danger" />
+                <MDBIcon 
+                  fas 
+                  icon="trash-alt" 
+                  color="danger" 
+                  onClick={() => handleDelete(id)} 
+                />
               </MDBTooltip>
             </div>
             <div className="text-end text-muted">
