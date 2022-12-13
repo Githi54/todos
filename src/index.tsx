a2175bdc772bc6c7999da1999e2dbcc5e777f4d6
Nikita Havryliuk <chinter.games1@gmail.com>
2022-12-13 17:30:03 +0200

    fixed index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import { App } from './App';
-import { TodoProvider } from './Components/Context/TodoContext';
 import { store } from './Redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <TodoProvider>
-      <App />
-    </TodoProvider>
+    <App />
   </Provider>,
   document.getElementById('root')
 );
